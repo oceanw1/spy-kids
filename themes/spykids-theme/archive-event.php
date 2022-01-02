2465bf4f495e9f59e25425c321e6ffcb4dede596
oceanw1 <79440854+oceanw1@users.noreply.github.com>
2022-01-03 00:15:59 +0700

    adding past events n stuff

--- themes/spykids-theme/archive-event.php
+++ themes/spykids-theme/archive-event.php
@@ -20,12 +20,31 @@
     <!-- 6 News articles -->
     <div id="page-of"  class="column">
     <?php
-      while(have_posts()) {
-        the_post();?>
+      $today = date('Ymd');
+      $archiveEvents = new WP_Query(array(
+        'orderby' => 'title',
+        'order' => 'meta_value',
+        'meta_key'=>'event_post_date',
+        'post_type' => 'event',
+        'meta_query'=>array(
+          array(
+            'key' => 'event_post_date',
+            'compare' => '>=',
+            'value' => $today,
+            'type' => 'numeric'
+          )
+        )
+      ));
+
+      while($archiveEvents->have_posts()) {
+        $archiveEvents->the_post();?>
         <div class="post-item">
           <h1 class="headline headline--small headline--post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
             <div class="metabox">
-              <p>Posted By <?php the_author_posts_link(); ?> on <?php the_time('M-j-Y'); ?></p>
+              <p>Posted By <?php the_author_posts_link(); ?> on <?php 
+                $eventDate = new DateTime(get_field('event_post_date'));
+                echo $eventDate->format('M-j-Y');
+              ?></p>
             </div>
             <div class="generic-content">
               <?php the_excerpt(); ?>
@@ -33,10 +52,13 @@
             </div>
         </div>
 
+        
   
     <?php  }
+    echo paginate_links();
     ?>
-
+  <hr class="section-break">
+      <p>Looking for the Past Events? <a href="<?php echo site_url('/past-events') ?>">Check out our past events</a></p>
 </section>
 
 <?php
